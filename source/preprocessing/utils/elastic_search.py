235320f830fd4ed6fd401f13d04f6d7d929ad1f6
goramartin <im.goramartin@gmail.com>
2023-11-18 18:53:00 +0100

    Scripts - Preprocessing - added auth on es

--- source/preprocessing/utils/elastic_search.py
+++ source/preprocessing/utils/elastic_search.py
@@ -1,8 +1,9 @@
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from utils.es_config import *
 
 CLASSES_ELASTIC_INDEX_NAME = "classes"
 PROPERTIES_ELASTIC_INDEX_NAME = "properties"
 CHUNK_SIZE = 10_000
 
-client = Elasticsearch("http://localhost:9200")
+client = Elasticsearch("https://localhost:9200", ca_certs=ES_CERT_PATH, basic_auth=('elastic', ES_PASSWD))
