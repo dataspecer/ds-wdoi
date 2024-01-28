e47a46e0dd13497fa01522af1f1634cc2e963216
goramartin <im.goramartin@gmail.com>
2024-01-28 11:00:31 +0100

    Frontend - Mockup - set up of proxies and query client

--- source/frontend/ds-mockup/.eslintrc.cjs
+++ source/frontend/ds-mockup/.eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'warn',
     'prettier/prettier': 'error',
   },
 };
