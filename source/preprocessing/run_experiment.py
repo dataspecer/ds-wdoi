3ab73745ccbdf1aa74a8a6eb98e86ee7b4e34c27
goramartin <im.goramartin@gmail.com>
2024-06-09 18:52:52 +0000

    Preprocessing - deleted call

--- source/preprocessing/run_experiment.py
+++ source/preprocessing/run_experiment.py
@@ -1,4 +1,2 @@
 from experiments.initial_search_class_selection.initial_search_class_selection import main_initial_search_class_selection
 
-
-main_initial_search_class_selection("./output/classes-recs.json", "./statistics-and-logs/with-diploma/classes_number_of_instances_and_normalize.json", "./statistics-and-logs/with-diploma/classes_ancestors_count.json", "./statistics-and-logs/with-diploma/classes_children_count.json")
